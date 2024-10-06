0403781d48434018e426ed7d37763060367dc354
User <User@DESKTOP-08VSUMT>
2024-10-06 21:17:51 +0300

    Proje dosyası ekle.

---
+++ TraversalApiProject/Controllers/VisitorController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TraversalApiProject.DAL.Context;
+using TraversalApiProject.DAL.Entities;
+
+namespace TraversalApiProject.Controllers
+{
+    [EnableCors]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VisitorController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult VisitorList()
+        {
+            using (var context = new VisitorContext())
+            {
+                var values = context.Visitors.ToList();
+                return Ok(values);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult VisitorAdd(Visitor visitor)
+        {
+            using (var context = new VisitorContext())
+            {
+                context.Add(visitor);
+                context.SaveChanges();
+                return Ok();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult VisitorGet(int id)
+        {
+            using (var context = new VisitorContext())
+            {
+                var values = context.Visitors.Find(id);
+                if (values == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(values);
+                }
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult VisitorDelete(int id)
+        {
+            using(var context = new VisitorContext())
+            {
+                var values = context.Visitors.Find(id);
+                if(values == null)
+                {
+                    return NotFound(values);
+                }
+                else
+                {
+                    context.Remove(values);
+                    context.SaveChanges();
+                    return Ok();
+                }
+            }
+        }
+
+        [HttpPut]
+        public IActionResult VisitorUpdate(Visitor visitor)
+        {
+            using ( var context = new VisitorContext())
+            {
+                var values = context.Visitors.Find(visitor.VisitorId);
+                if( values == null)
+                {
+                    return NotFound(values);
+                }
+                else
+                {
+                    values.VisitorName = visitor.VisitorName;
+                    values.VisitorSurname = visitor.VisitorSurname;
+                    values.VisitorMail = visitor.VisitorMail;
+                    values.VisitorCity = visitor.VisitorCity;
+                    values.VisitorCountry = visitor.VisitorCountry;
+                   
+                    context.Update(values);
+                    context.SaveChanges();
+                    return Ok();
+                }
+            }
+        }
+    }
+}
---
+++ TraversalApiProject/Migrations/20241006073412_create_first_migration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TraversalApiProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class create_first_migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Visitors",
+                columns: table => new
+                {
+                    VisitorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VisitorName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VisitorSurname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VisitorCity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VisitorCountry = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VisitorMail = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visitors", x => x.VisitorId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Visitors");
+        }
+    }
+}
